093ab414e880179316191c8ff31632de0984feb7
vanyadeg <id1997@yandex.ru>
2017-09-16 10:39:22 +0300

    LAB-1: improved test logging

--- SELab1/src/test/java/degtiarenko/hw/cache/LRUCacheImplTest.java
+++ SELab1/src/test/java/degtiarenko/hw/cache/LRUCacheImplTest.java
@@ -7,6 +7,9 @@ import static org.junit.Assert.assertEquals;
 
 public class LRUCacheImplTest {
 
+    private static final String VALUE_DIFFERS_MSG = "Value differs from what expected.";
+    private static final String VALUE_NOT_EXPECTED_MSG = "Value is not expected to be present.";
+
     private Cache<Integer, String> namesCache;
 
     @Before
@@ -16,15 +19,15 @@ public class LRUCacheImplTest {
 
     @Test
     public void testGet() {
-        Cache<Integer, String> namesCache = new LRUCacheImpl<>(100);
+        namesCache = new LRUCacheImpl<>(100);
 
         namesCache.put(1, "Ivan");
         namesCache.put(2, "Ksyusha");
         namesCache.put(3, "Leha");
 
-        assertEquals("Ivan", namesCache.get(1));
-        assertEquals("Ksyusha", namesCache.get(2));
-        assertEquals("Leha", namesCache.get(3));
+        assertEquals(VALUE_DIFFERS_MSG, "Ivan", namesCache.get(1));
+        assertEquals(VALUE_DIFFERS_MSG, "Ksyusha", namesCache.get(2));
+        assertEquals(VALUE_DIFFERS_MSG, "Leha", namesCache.get(3));
     }
 
     @Test
@@ -34,8 +37,8 @@ public class LRUCacheImplTest {
         namesCache.put(1, "a");
         namesCache.put(2, "b");
 
-        assertEquals("a", namesCache.put(1, "c"));
-        assertEquals(null, namesCache.put(3, "d"));
+        assertEquals(VALUE_DIFFERS_MSG, "a", namesCache.put(1, "c"));
+        assertEquals(VALUE_NOT_EXPECTED_MSG, null, namesCache.put(3, "d"));
     }
 
     @Test
@@ -47,22 +50,22 @@ public class LRUCacheImplTest {
         namesCache.put(3, "c");
         namesCache.put(4, "d");
 
-        assertEquals(null, namesCache.get(1));
+        assertEquals(VALUE_NOT_EXPECTED_MSG, null, namesCache.get(1));
 
         namesCache.put(5, "e");
 
-        assertEquals(null, namesCache.get(2));
+        assertEquals(VALUE_NOT_EXPECTED_MSG, null, namesCache.get(2));
 
         namesCache.put(6, "sixth letter");
         namesCache.put(7, "seventh letter");
         namesCache.put(8, "eighth letter");
 
-        assertEquals(null, namesCache.get(3));
-        assertEquals(null, namesCache.get(4));
-        assertEquals(null, namesCache.get(5));
-        assertEquals("sixth letter", namesCache.get(6));
-        assertEquals("seventh letter", namesCache.get(7));
-        assertEquals("eighth letter", namesCache.get(8));
+        assertEquals(VALUE_NOT_EXPECTED_MSG, null, namesCache.get(3));
+        assertEquals(VALUE_NOT_EXPECTED_MSG, null, namesCache.get(4));
+        assertEquals(VALUE_NOT_EXPECTED_MSG, null, namesCache.get(5));
+        assertEquals(VALUE_DIFFERS_MSG, "sixth letter", namesCache.get(6));
+        assertEquals(VALUE_DIFFERS_MSG, "seventh letter", namesCache.get(7));
+        assertEquals(VALUE_DIFFERS_MSG, "eighth letter", namesCache.get(8));
     }
 
     @Test
@@ -75,9 +78,9 @@ public class LRUCacheImplTest {
         namesCache.put(1, "c");
         namesCache.put(3, "d");
 
-        assertEquals("c", namesCache.get(1));
-        assertEquals(null, namesCache.get(2));
-        assertEquals("d", namesCache.get(3));
+        assertEquals(VALUE_DIFFERS_MSG, "c", namesCache.get(1));
+        assertEquals(VALUE_NOT_EXPECTED_MSG, null, namesCache.get(2));
+        assertEquals(VALUE_DIFFERS_MSG, "d", namesCache.get(3));
     }
 
     @Test
@@ -88,10 +91,10 @@ public class LRUCacheImplTest {
         namesCache.put(2, "b");
 
         namesCache.get(1);
-        assertEquals(null, namesCache.put(3, "c"));
+        assertEquals(VALUE_NOT_EXPECTED_MSG, null, namesCache.put(3, "c"));
 
-        assertEquals("a", namesCache.get(1));
-        assertEquals(null, namesCache.get(2));
-        assertEquals("c", namesCache.get(3));
+        assertEquals(VALUE_DIFFERS_MSG, "a", namesCache.get(1));
+        assertEquals(VALUE_NOT_EXPECTED_MSG, null, namesCache.get(2));
+        assertEquals(VALUE_DIFFERS_MSG, "c", namesCache.get(3));
     }
 }

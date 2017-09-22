7258e3a48e36ac71090e2318b4b6eb5f03628bd4
degivan <id1997@yandex.ru>
2017-09-22 16:14:36 +0300

    LAB-2: created wrapper for JSON tweet

---
+++ SELab2/src/main/java/degtiarenko/hw/tweets/TweetList.java
@@ -0,0 +1,32 @@
+package degtiarenko.hw.tweets;
+
+import org.json.JSONArray;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class TweetList {
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM d hh:mm:ss ZZZZZ yyyy", Locale.ENGLISH);
+    private final JSONArray tweets;
+
+    public TweetList(JSONArray tweets) {
+        this.tweets = tweets;
+        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
+
+    public int getListLength() {
+        return tweets.length();
+    }
+
+    public Date getTweetDate(int index) throws ParseException {
+        String date = tweets.getJSONObject(index).getString("created_at");
+        return dateFormat.parse(date);
+    }
+
+    public String getId(int index) {
+        return tweets.getJSONObject(index).getString("id");
+    }
+}

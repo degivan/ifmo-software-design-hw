caba0efe776587a210dfa6f3161156c3cf037145
vanyadeg <id1997@yandex.ru>
2017-09-30 05:04:15 +0300

    LAB-2: fixed results for hours without tweets

--- SELab2/src/main/java/degtiarenko/hw/tweets/StatisticServiceImpl.java
+++ SELab2/src/main/java/degtiarenko/hw/tweets/StatisticServiceImpl.java
@@ -5,11 +5,12 @@ import org.joda.time.DateTime;
 import org.joda.time.Hours;
 
 import java.text.ParseException;
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class StatisticServiceImpl implements StatisticService {
 
@@ -21,13 +22,15 @@ public class StatisticServiceImpl implements StatisticService {
 
     @Override
     public List<Long> getHashTagPopularity(String hashTag, int hours) throws UnirestException, ParseException {
-        DateTime lastTime = new DateTime().minusHours(hours);
+        List<Long> result = new ArrayList<>();
+        DateTime currentTime = new DateTime();
+        DateTime lastTime = currentTime.minusHours(hours);
         Map<Integer, Long> tweetsPerHour = searchService.getTweetsWithHashTag(hashTag, lastTime).stream()
                 .filter(tweet -> tweet.getDate().isAfter(lastTime))
-                .collect(groupingBy(tweet -> Hours.hoursBetween(tweet.getDate(), lastTime).getHours(), counting()));
-        return tweetsPerHour.entrySet().stream()
-                .sorted(Comparator.comparingInt(Map.Entry::getKey))
-                .map(Map.Entry::getValue)
-                .collect(toList());
+                .collect(groupingBy(tweet -> Hours.hoursBetween(tweet.getDate(), currentTime).getHours(), counting()));
+        for (int i = 0; i < hours; i++) {
+            result.add(tweetsPerHour.getOrDefault(i, 0L));
+        }
+        return result;
     }
 }
